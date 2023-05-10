1cb939cdca2834eb3e5b6a3807378c25156e898d
luanfv <luanfrancavieira@gmail.com>
2023-05-10 16:13:58 -0300

    test: /games/:gameId/finish

--- src/game/controllers/game.controller.integration.spec.ts
+++ src/game/controllers/game.controller.integration.spec.ts
@@ -4,6 +4,7 @@ import * as request from 'supertest';
 
 import { GameModule } from '../game.module';
 import { GameRepository } from '../repositories/game.repository';
+import { prisma } from '../../databases/prisma';
 
 describe('GameController (Integration)', () => {
   let app: INestApplication;
@@ -87,6 +88,101 @@ describe('GameController (Integration)', () => {
         await request(app.getHttpServer())
           .put('/games/id-invalid/restart')
           .expect(HttpStatus.NOT_FOUND);
+      });
+    });
+  });
+
+  describe('PUT /games/:gameId/finish', () => {
+    describe('WHEN cannot finish the game because game is not found', () => {
+      it('SHOULD return NOT FOUND', async () => {
+        await request(app.getHttpServer())
+          .put('/games/id-invalid/finish')
+          .expect(HttpStatus.NOT_FOUND);
+      });
+    });
+
+    describe('WHEN cannot finish the game because game is already finished', () => {
+      it('SHOULD return BAD REQUEST', async () => {
+        const foundGame = await gameRepository.create();
+        await prisma.game.update({
+          data: {
+            is_game_over: true,
+          },
+          where: {
+            id: foundGame.id,
+          },
+        });
+
+        await request(app.getHttpServer())
+          .put(`/games/${foundGame.id}/finish`)
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+
+    describe('WHEN cannot finish the game because game not enough players', () => {
+      it('SHOULD return BAD REQUEST', async () => {
+        const foundGame = await gameRepository.create();
+        await request(app.getHttpServer())
+          .put(`/games/${foundGame.id}/finish`)
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+
+    describe('WHEN cannot finish the game because game not all players pick', () => {
+      it('SHOULD return BAD REQUEST', async () => {
+        const foundGame = await gameRepository.create();
+        await prisma.player.create({
+          data: {
+            game_id: foundGame.id,
+            username: 'Player1',
+            id: '1',
+          },
+        });
+        await prisma.player.create({
+          data: {
+            game_id: foundGame.id,
+            username: 'Player2',
+            id: '2',
+            pick: 'JO',
+          },
+        });
+
+        await request(app.getHttpServer())
+          .put(`/games/${foundGame.id}/finish`)
+          .expect(HttpStatus.BAD_REQUEST);
+      });
+    });
+
+    describe('WHEN can finish the game', () => {
+      it('SHOULD return OK with the game', async () => {
+        const foundGame = await gameRepository.create();
+        await prisma.player.create({
+          data: {
+            game_id: foundGame.id,
+            username: 'Player1',
+            id: '3',
+            pick: 'KEN',
+          },
+        });
+        await prisma.player.create({
+          data: {
+            game_id: foundGame.id,
+            username: 'Player2',
+            id: '4',
+            pick: 'JO',
+          },
+        });
+
+        const result = await request(app.getHttpServer())
+          .put(`/games/${foundGame.id}/finish`)
+          .expect(HttpStatus.OK);
+
+        expect(result.body).toEqual({
+          id: foundGame.id,
+          is_game_over: true,
+          winner_id: '3',
+          created_at: expect.anything(),
+        });
       });
     });
   });
